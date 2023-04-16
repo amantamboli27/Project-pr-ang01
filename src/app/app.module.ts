978d47e5c616a15fb0a7f33987687790c0b4cfa0
Aman Tamboli <amant@centiro.se>
2023-04-16 22:10:55 +0530

    Commit(1):Login-Notify

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { NotifyComponent } from './notify/notify.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    NotifyComponent
   ],
   imports: [
     BrowserModule,
